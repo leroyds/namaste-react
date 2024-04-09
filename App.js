541f255e4e8d47025cc6c87ba4a1c6e8c0fdb475
leroyturtlemint <leroy.saldanha@turtlemint.com>
2024-04-09 13:15:09 +0530

    ep 02

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
 const parent = React.createElement("div", {id: 'header'}, [
     React.createElement("div", {id:'child'}, [
         React.createElement("h1", {}, 'heading 1'),
